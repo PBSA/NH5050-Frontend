ad53d4292ded9210f9dcf2c6cc6a02cf8ac3913c
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2020-04-14 16:25:49 +0300

    refactor admin table, implement tickets page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Dashboard from '../components/Dashboard';
 import AdminLogin from '../components/AdminLogin';
 import AdminDashboard from '../components/AdminDashboard';
+import AdminTicketDetails from '../components/AdminTicketDetails';
 import SellerForm from '../components/SellerForm';
 import CheckoutContainer from '../components/CheckoutContainer';
 import { RouteConstants as Routes } from '../constants';
@@ -16,6 +17,7 @@ const routes = (
       <Route path={Routes.CONFIRMATION_PAGE} component={CheckoutContainer} />
       <Route path={Routes.GROW_JACKPOT} component={CheckoutContainer} />
       <Route path={Routes.ADMIN_LOGIN} component={AdminLogin} />
+      <Route path={`${Routes.ADMIN_TICKETS}/:ticket_id`} component={AdminTicketDetails} />
       <Route path={Routes.ADMIN} component={AdminDashboard} />
       <Route path={Routes.ADMIN_EDIT_SELLER} component={SellerForm} />
     </Switch>
