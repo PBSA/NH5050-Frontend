ec005742902c4444dda7292694f9d7cab68b7cc3
Adrian <a.metzler@pbsa.info>
2020-04-15 09:38:27 -0300

    added dashboard raffle stats

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -4,6 +4,7 @@ import {
 } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import moment from 'moment';
 import { NavigateActions } from '../../redux/actions';
 import strings from '../../assets/locales/strings';
 
@@ -12,6 +13,7 @@ import Sellers from './Sellers';
 import Raffles from './Raffles';
 import Tickets from './Tickets';
 import { RouteConstants } from '../../constants';
+import { RaffleService } from '../../services';
 
 const tabs = [
   { id: 'beneficiaries', label: strings.adminDashboard.tabs.beneficiaries, route: RouteConstants.ADMIN_BENEFICIARIES },
@@ -33,18 +35,53 @@ class AdminDashboard extends Component {
 
       tabIndex++;
     }
-    this.state = { tabIndex };
+    this.state = { 
+      tabIndex,
+      progressiveRaffle: {},
+      timeToDraw: '',
+      timeToProgressiveDraw: '',
+     };
   }
 
   setTabIndex = (index) => {
     this.setState({tabIndex: index});
   }
 
-  render() {
-    const { organizationId } = this.props;
-    const { tabIndex } = this.state;
-    const activeTab = tabIndex < 4 ? tabs[tabIndex].id : false;
+  tick = () => {
+    this.setState({
+      timeToDraw: this.timeToDraw(this.state.raffle.draw_datetime),
+      timeToProgressiveDraw: this.timeToDraw(this.state.progressive.draw_datetime)
+    });
+  }
 
+  timeToDraw = (drawdate) => {
+    const diff = moment.duration(moment(drawdate).diff(moment()));
+
+    let days = moment(drawdate).diff(moment(), 'days');
+    return `${days}d ${diff.hours()}h ${diff.minutes()}m ${diff.seconds()}s`;
+  }
+
+  getProgressiveRaffle() {
+    const progressiveId = this.props.raffle.progressive_draw_id
+    RaffleService.getRaffleById(progressiveId).then((progressiveRaffle) => {
+      console.log('PROGRESSIVE RAFFLE: ', progressiveRaffle);
+      this.setState({progressiveRaffle});
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    
+    if(prevProps.raffle !== this.props.raffle) {
+      this.getProgressiveRaffle()
+    }
+  }
+
+  render() {
+    const { organizationId, raffle } = this.props;
+    const { tabIndex, progressiveRaffle, timeToDraw, timeToProgressiveDraw } = this.state;
+    const activeTab = tabIndex < 4 ? tabs[tabIndex].id : false;
+    console.log('admin dashboard: ',this.props);
+    debugger;
     return (
       <Card className="admin" variant="outlined">
         <CardContent>
@@ -55,7 +92,22 @@ class AdminDashboard extends Component {
           {activeTab === 'sellers' && <Sellers organizationId={organizationId} />}
           {activeTab === 'raffles' && <Raffles organizationId={organizationId} />}
           {activeTab === 'tickets' && <Tickets organizationId={organizationId} />}
-          {activeTab === false ? <h1>Admin Dashboard Goes Here!</h1> : null}
+          {activeTab === false ? 
+            <div className="confirmation-jackpots">
+              <div className="confirmation-jackpots-5050">
+                <span className="confirmation-jackpots-header"> {strings.confirmationPage.jackpot} </span>
+                <span className="confirmation-jackpots-amount"> ${raffle.total_jackpot} </span>
+                <span className="confirmation-jackpots-header"> {timeToDraw} </span>
+                <span className="confirmation-jackpots-date"> {strings.confirmationPage.drawn} {moment(raffle.draw_datetime).format('ha ddd, MMM D, YYYY')} </span>
+              </div>
+              <div className="confirmation-jackpots-progressive">
+                <span className="confirmation-jackpots-header"> {strings.confirmationPage.progressivejackpot} </span>
+                <span className="confirmation-jackpots-amount"> ${progressiveRaffle.total_progressive_jackpot} </span>
+                <span className="confirmation-jackpots-header"> {timeToProgressiveDraw} </span>
+                <span className="confirmation-jackpots-date"> {strings.confirmationPage.drawn} {moment(progressiveRaffle.draw_datetime).format('ha ddd, MMM D, YYYY')} </span>
+              </div>
+            </div>
+          : null}
         </CardContent>
       </Card>
     );
@@ -66,6 +118,8 @@ const mapStateToProps = (state) => {
   return {
     organizationId: state.getIn(['checkout', 'organizationId']),
     path: state.getIn(['router', 'location', 'pathname']),
+    organization: state.getIn(['checkout', 'organization']),
+    raffle: state.getIn(['checkout', 'raffle'])
   };
 };
 
